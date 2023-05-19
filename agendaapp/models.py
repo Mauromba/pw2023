2ecd9dea63da714117c989b205074b70b046b632
Mauromba <mbaiaoneto@gmail.com>
2023-05-19 20:15:04 -0300

    first commit

---
+++ agendaapp/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Servico(models.Model):
+    nome_servico = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome_servico
+
+
+class Agenda(models.Model):
+    data_agenda = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.data_agenda
