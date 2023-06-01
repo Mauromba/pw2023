bf8760c3c0aa49b15540273eae990e3bd8dc2e6b
Mauromba <mbaiaoneto@gmail.com>
2023-06-01 10:19:24 -0300

    Atualização

--- agenda/urls.py
+++ agenda/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from agendaapp.views import index, cad_servico
+from agendaapp.views import base03, base03
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', index, name='index'),
     path('cad_servico/', cad_servico, name='cad_servico'),
+    path ('base03/', base03, name='base03'),
 ]
--- agendaapp/views.py
+++ agendaapp/views.py
@@ -6,6 +6,9 @@ from .forms import Servicoform, Agendaform
 def index(request):
     return render(request,'agenda/index.html')
 
+def base03(request):
+    return render(request,'agenda/base03.html')
+
 def cad_servico(request):
     form = Servicoform()
     return render(request, 'agenda/criar_serv.html', {'form':form})
